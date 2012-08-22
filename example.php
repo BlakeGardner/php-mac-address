de6071e3cc26398576d16a511b6ecc4a2322e3cb
Blake Gardner <blakegardner@cox.net>
2012-08-22 18:49:07 -0500

    Updated example file to use the static class methods

--- example.php
+++ example.php
@@ -3,23 +3,19 @@
 // require the class
 require_once 'php-mac-address.php';
 
-// initialize the class
-$mac = new MAC_Address;
-
 // get the mac address of the 'en1' interface
-echo "Current MAC address: " . $mac->get_current_mac_address('en1') . "\n\n";
+echo "Current MAC address: " . MAC_Address::get_current_mac_address('en1') . "\n\n";
 
 // generate a random mac address
-echo "Generated MAC address: " . $mac->generate_mac_address() . "\n\n";
+echo "Generated MAC address: " . MAC_Address::generate_mac_address() . "\n\n";
 
 $valid_mac    = '00-B0-D0-86-BB-F7';
 $invalid_mac  = '00-B0-D0-86-BB-FG';
 
 echo "Validating {$valid_mac}... Result: ";
-var_dump($mac->validate_mac_address($valid_mac));
+var_dump(MAC_Address::validate_mac_address($valid_mac));
 
 echo "\n";
 
 echo "Validating {$invalid_mac}... Result: ";
-var_dump($mac->validate_mac_address($invalid_mac));
-
+var_dump(MAC_Address::validate_mac_address($invalid_mac));
