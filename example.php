8fe1020c552784a0c73c90bd41186460e4f733c7
Blake Gardner <blakegardner@cox.net>
2013-05-18 15:59:43 -0500

    Updated example PHP file to reflect the new class name, name space, and method names

--- example.php
+++ example.php
@@ -1,30 +1,34 @@
 <?php
 
-// require the class
-require_once 'php-mac-address.php';
 echo "\n";
+
+// require the class
+require_once './src/BlakeGardner/MacAddress.php';
+
+// import the class
+use BlakeGardner\MacAddress;
 
 // get the mac address of the eth0 interface
 echo "Current MAC address: ";
-var_dump(MAC_Address::get_current_mac_address('eth0'));
+var_dump(MacAddress::getCurrentMacAddress('eth0'));
 echo "\n";
 
 // generate a random mac address
 echo "Randomly generated MAC address: ";
-var_dump(MAC_Address::generate_mac_address());
+var_dump(MacAddress::generateMacAddress());
 echo "\n";
 
 // validate an MAC address
 echo "Validating MAC address: ";
-var_dump(MAC_Address::validate_mac_address('00-B0-D0-86-BB-F7'));
+var_dump(MacAddress::validateMacAddress('00-B0-D0-86-BB-F7'));
 echo "\n";
 
 // set a randomly generated MAC address on the eth0 interface
 echo "Setting a randomly generated MAC address: ";
-var_dump(MAC_Address::set_fake_mac_address('eth0'));
+var_dump(MacAddress::setFakeMacAddress('eth0'));
 echo "\n";
 
 // set a specific MAC address on the eth0 interface
 echo "Setting a specific MAC address: ";
-var_dump(MAC_Address::set_fake_mac_address('eth0', '00:E4:01:2C:79:DA'));
+var_dump(MacAddress::setFakeMacAddress('eth0', '00:E4:01:2C:79:DA'));
 echo "\n";
